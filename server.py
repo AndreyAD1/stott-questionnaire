5d24614daa324b3a7def4716a8672c57a56cf108
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-08-14 15:31:37 +0300

    Begin to make Flask application.

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def index():
+    return render_template('index.html', page_title='Главная')
+
+
+@application.route('/person_info')
+def person_info():
+    return render_template(
+        'person_info.html',
+        page_title='Информация об обследуемом'
+    )
+
+
+if __name__ == '__main__':
+    application.run()
