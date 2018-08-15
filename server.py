02c8fceaf772bc602ec4c1c68fc0cf525622777a
AndreyAD1 <arakelyants.andrey@yandex.ru>
2018-08-15 15:08:52 +0300

    Improve input forms and get input data on the server.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 
@@ -6,6 +6,7 @@ application = Flask(__name__)
 application.config.update(ENV='development', DEBUG=True)
 with open('items.json', 'r', encoding='utf-8') as item_file:
     item_list = json.load(item_file)
+
 
 
 @application.route('/')
@@ -23,18 +24,19 @@ def person_info():
 
 @application.route('/questions/<item_num>', methods=['POST'])
 def questions_and_result(item_num):
+    print(request.form)
     page_number = int(item_num)
     if page_number <= len(item_list):
         item_index = page_number - 1
         subitem = item_list[item_index]
         total_page_number = len(item_list)
-        next_page_number = page_number + 1
+        next_page_name = page_number + 1
         return render_template(
                 '_questions.html',
                 page_content=subitem,
                 page_number=page_number,
                 total_page_number=total_page_number,
-                next_page_number=next_page_number
+                next_page_number=next_page_name
             )
     if page_number > len(item_list):
         return render_template('result.html')
